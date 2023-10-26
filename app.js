f913a97b9a1aa7cb4e83479938ad626a86ce0850
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-10-25 22:15:31 -0400

    do

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 
-const DOMSelectors ={
-    button: document.getElementById("btn"), 
-    text: document.querySelector("#text"), 
-    box: document.getElementById("big-black-box"), 
-    point: document.querySelectorAll(".point"),
-};
+// const DOMSelectors ={
+//     button: document.getElementById("btn"), 
+//     text: document.querySelector("#text"), 
+//     box: document.getElementById("big-black-box"), 
+//     point: document.querySelectorAll(".point"),
+// };
 
-function backgroundAndText (background, text){
-    background.style.backgroundColor ="red";
-    text.innerHTML ="tgus us biw bugr red"
-}
+// function backgroundAndText (background, text){
+//     background.style.backgroundColor ="red";
+//     text.innerHTML ="tgus us biw bugr red"
+// }
 
-DOMSelectors.button.addEventListener("click", function(){
-    backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-})
+// DOMSelectors.button.addEventListener("click", function(){
+//     backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+// })
 
-function changeLi(){
-    let pointIndec = 1; 
-    DOMSelectors.point.forEach((point) => {
-        point.addEventListener("click", function() {
-            point.textContent = `hello ${pointIndec}`; 
-        });
-    });
-}
+// function changeLi(){
+//     let pointIndec = 1; 
+//     DOMSelectors.point.forEach((point) => {
+//         point.addEventListener("click", function() {
+//             point.textContent = `hello ${pointIndec}`; 
+//         });
+//     });
+// }
 
-changeLi(); 
+// changeLi(); 
 
  
